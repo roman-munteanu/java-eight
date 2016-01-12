dd16e5963acf3c5df14c455e6a56dc0b1d87e2ca
Roman Munteanu <Roman.Munteanu@endava.com>
2016-01-12 12:15:01 +0200

    package for lambdas

---
+++ src/com/munteanu/lambdas/MainLambdas.java
@@ -0,0 +1,10 @@
+package com.munteanu.lambdas;
+
+/**
+ * Created by romunteanu on 1/12/2016.
+ */
+public class MainLambdas {
+    public static void main(String[] args) {
+        System.out.println("Java 8!");
+    }
+}
